1e20e472cb6f4324815545f5e57b2853a6312437
cristiii <cristian.vlad10@gmail.com>
2023-03-25 09:09:23 +0200

    added couple of tests

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    private IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", indexController.index());
+    }
+
+    @Test
+    void oupsHandler() {
+        assertEquals("notimplemented", indexController.oupsHandler(), () -> "this is some expensive message to build for test");
+    }
+}
---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Joe", "Buck");
+        owner.setCity("City");
+        owner.setTelephone("123123");
+
+        assertAll("Properties test",
+                () -> assertAll("Person properties",
+                        () -> assertEquals("Joe", owner.getFirstName()),
+                        () -> assertEquals("Buck", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("City", owner.getCity()),
+                        () -> assertEquals("123123", owner.getTelephone())));
+    }
+
+}
